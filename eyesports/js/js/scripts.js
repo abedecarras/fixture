94a8a65a5ce4f6d32aea17d54c2cc086af13bf75
Agustin-PC\Agustin <abedecarras@gmail.com>
2018-06-18 10:26:52 -0300

    changes on script.js

--- eyesports/js/js/scripts.js
+++ eyesports/js/js/scripts.js
@@ -1,18 +1,18 @@
-var services = new ProdeServices();
+  var services = new ProdeServices();
 
-var printResult = function(response) {
-    // document.getElementsByTagName('textarea')[0].innerHTML = JSON.stringify(response);
-    // console.log(JSON.stringify(response));
-    console.log('printResult');
-    console.log(response);
-};
+  var printResult = function(response) {
+      // document.getElementsByTagName('textarea')[0].innerHTML = JSON.stringify(response);
+      // console.log(JSON.stringify(response));
+      console.log('printResult');
+      console.log(response);
+  };
 
-var validation = function(){
-  let username = document.forms['myForm']['fname'].value;
-  let password = document.forms['myForm']['fpassword'].value;
+  var validation = function(){
+    let username = document.forms['myForm']['fname'].value;
+    let password = document.forms['myForm']['fpassword'].value;
 
-  services.login(username, password, loggedResponse);
-}
+    services.login(username, password, loggedResponse);
+  }
 
   var loggedResponse = function(response) {
     console.log(response.logged);
@@ -20,7 +20,13 @@ var validation = function(){
       window.location.href="home.html";
     }
     else {
-      // console.log(response);
-      // console.log('usuario o contraseña incorrecto');
+      console.log(response);
+      console.log('usuario o contraseña incorrecto');
+      let inputs = document.querySelectorAll('.input');
+      for (var item of inputs) {
+        // console.log(item);
+        item.style.borderColor = '#8b0300';
+      }
+      document.querySelector('.error').className = 'e-show';
     }
   }
